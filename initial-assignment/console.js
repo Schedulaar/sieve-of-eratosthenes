57e102eba13ee6f8847cba9eda59278ff2fd14ea
Michael Markl <marklmichael98@gmail.com>
2020-10-13 21:30:17 +0200

    Add js scripts

---
+++ initial-assignment/console.js
@@ -0,0 +1,32 @@
+var groupBy = function(xs, key) {
+  return xs.reduce(function(rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
+};
+
+const pData = groupBy(data, 'p')
+
+function objectMap(object, mapFn) {
+  return Object.keys(object).reduce(function(result, key) {
+    result[key] = mapFn(object[key])
+    return result
+  }, {})
+}
+
+const procNData = objectMap(pData, val => groupBy(val, 'n'))
+
+const meansAndDeviations = objectMap(procNData, pdata => objectMap(pdata, iterations => {
+  const mean = iterations.reduce((acc,val) => acc + val.t,0)/iterations.length
+  const sum = iterations.reduce((acc,val) => acc + (val.t - mean)*(val.t - mean),0)
+  const relDeviation = Math.sqrt(sum / (iterations.length - 1)) / mean
+  return { mean, relDeviation }
+}))
+
+const latexData = objectMap(meanAndDeviationObj2, pdata => {
+  const means = Object.keys(pdata).reduce((acc, n) => acc += `(${n}, ${pdata[n].mean})
+`, '')
+  const relDeviations = Object.keys(pdata).reduce((acc, n) => acc += `(${n}, ${pdata[n].relDeviation})
+`, '')
+  return { means, relDeviations }
+})
